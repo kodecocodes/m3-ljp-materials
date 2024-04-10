949b88633cc62a82f72fe9dedca70f58c320d9e2
Kevin Moore <kevindavmoore@gmail.com>
2024-04-10 09:18:30 -0700

    TE Review

--- 01-use-scrolling-modifiers/Starter/app/src/main/java/com/yourcompany/android/githubusers/MainActivity.kt
+++ 01-use-scrolling-modifiers/Starter/app/src/main/java/com/yourcompany/android/githubusers/MainActivity.kt
@@ -44,7 +44,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.yourcompany.android.githubusers.components.GitHubRepoCard
